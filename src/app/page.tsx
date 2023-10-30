fa1c53b401590559276ebacd42eca22bea9c8d3b
John Maguire <3673943+jonniedarko@users.noreply.github.com>
2023-10-30 10:07:47 -0400

    changes saved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,20 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import { Container, Typography, Button, Box } from '@mui/material';
-import Link from 'next/link'
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Container, Typography, Button, Box } from "@mui/material";
+import Link from "next/link";
 
 const LandingPage = () => {
-  
   return (
     <Container maxWidth="md">
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          minHeight: '100vh',
-          textAlign: 'center',
-          bgcolor: 'background.default',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "100vh",
+          textAlign: "center",
+          bgcolor: "background.default",
         }}
       >
         <Typography variant="h2" gutterBottom>
@@ -25,12 +24,10 @@ const LandingPage = () => {
           Monitor and manage your parking sessions easily
         </Typography>
         <Box sx={{ mt: 4 }}>
-          <Link href="/register">
-            
-          
-          <Button variant="contained" color="primary" size="large">
-            Get Started
-          </Button>
+          <Link href="/login">
+            <Button variant="contained" color="primary" size="large">
+              Get Started
+            </Button>
           </Link>
         </Box>
       </Box>
@@ -38,4 +35,5 @@ const LandingPage = () => {
   );
 };
 
-export default LandingPage;``
+export default LandingPage;
+``;
