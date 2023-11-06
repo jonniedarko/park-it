c43f9bc6edb8aef72ce706473ae5fd7579985304
John Maguire <3673943+jonniedarko@users.noreply.github.com>
2023-11-06 11:30:53 -0500

    changes saved

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,8 +1,7 @@
 import register from "@/firebase/auth/regsiter";
 import { NextRequest } from "next/server";
-const ALLOWED_DOMAINS = ['jonnie.io']
+const ALLOWED_DOMAINS = ["jonnie.io"];
 export async function POST(request: NextRequest) {
-  
   try {
     const body = await request.json();
     if (!body.email || !body.password) {
@@ -15,22 +14,22 @@ export async function POST(request: NextRequest) {
         },
       );
     }
-    const emailDomain = body.email.split('@')[0];
-    if(ALLOWED_DOMAINS.includes(emailDomain){
+    const emailDomain = body.email.split("@")[1];
+    if (ALLOWED_DOMAINS.includes(emailDomain)) {
       await register(body.email, body.password);
 
       return Response.json({
         success: true,
       });
-    } 
+    }
     return Response.json(
-        {
-          error: "NOT ALLOWED",
-        },
-        {
-          status: 401,
-        },
-      );
+      {
+        error: "NOT ALLOWED",
+      },
+      {
+        status: 401,
+      },
+    );
   } catch (e) {
     return Response.json(
       { success: true, error: null },
